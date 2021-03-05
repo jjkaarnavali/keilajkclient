ee87489fa4d2ae7748b5825f8e8fd249362109d9
jakaar <jakaar@ttu.ee>
2021-03-05 10:01:15 +0200

    added 3 sec countdown

--- src/controllers/game-controller.js
+++ src/controllers/game-controller.js
@@ -10,7 +10,7 @@ function animate(rowIndex, content, colCount){
         let colWidth = window.innerWidth / colCount;
 
         let pathPosition = Number(content.lastElementChild.lastElementChild.dataset.pathPosition);
-        console.log(content.lastElementChild.lastElementChild);
+        //console.log(content.lastElementChild.lastElementChild);
         let pathChange = Math.random() * (pathWidth - 3);
         pathChange = Math.ceil((Math.random() * 2 - 1.5)) * pathChange;
         if(pathPosition + pathChange + pathWidth >= rowCount || pathPosition + pathChange < 1){
@@ -75,84 +75,45 @@ export default class GameController {
 
         this.viewContainer.append(this.getBoardHtml(this.model));
         // start countdown
+
+        
+        
+    
+
+        
+        
+        
+        let timeleft = 3;
+        let downloadTimer = setInterval(function(){
+        let original = document.getElementById("view-container").firstElementChild.firstElementChild.firstElementChild.innerHTML;
+        if(timeleft <= 0){
+            clearInterval(downloadTimer);
+            
+            document.getElementById("view-container").firstElementChild.getElementsByClassName('row-7')[0].childNodes[10].innerHTML = original;      
+            //console.log(document.getElementById("view-container").firstElementChild.getElementsByClassName('row-0')[0].childNodes[5]);
+                
+        } else {
+            //console.log(document.getElementById("view-container").firstElementChild.getElementsByClassName('row-0')[0].childNodes[5]);
+            document.getElementById("view-container").firstElementChild.getElementsByClassName('row-7')[0].childNodes[10].innerHTML = timeleft;      
+        }
+        timeleft -= 1;
+        
+        }, 900);
+        
         
        
 
         // start game-loop
-        let rowIndex = 100;
-        animate(rowIndex, this.viewContainer.firstElementChild, this.model.colCount);
-        /*let rowIndex = 20;
-
+        let first = this.viewContainer.firstElementChild;
+        let count = this.model.colCount;
+        setTimeout(function() {
+            let rowIndex = 100;
+            animate(rowIndex, first, count);
+        }, 3500);
         
-            let viewPortHeight = window.innerHeight - 21;
-            let rowHeight = viewPortHeight / 20;
-            let colWidth = window.innerWidth / this.model.colCount;
-            let pathPosition = this.viewContainer.firstElementChild.lastElementChild.lastElementChild.dataset.pathPosition;
-            setTimeout(() => {
-                while (rowIndex < 50) {
-                
-                let colIn = 20;
-        
-                
-                
-                let pathChange = Math.random() * (5 - 3);
-                pathChange = Math.ceil((Math.random() * 2 - 1.5)) * pathChange;
-                console.log(pathPosition);
-                if(pathPosition + pathChange + 5 >= 20 || pathPosition + pathChange < 1){
-                    pathPosition = pathPosition;
-                }else{
-                    pathPosition = pathPosition + pathChange;
-                }
-                
-                
-        
-                for (let index = 0; index < 20; index++) {
-                    let c = this.viewContainer.firstElementChild.getElementsByClassName('row-' + index)[0];
-        
-                    
-                    if(c.firstElementChild !== null){
-                        c.firstElementChild.remove();
-                    }
-                    
-                    
-        
-                    let colElem = document.createElement('div');
-                    colElem.classList.add('id-row-' + index +'-col-' + colIn);
-                    
-                    
-                    
-        
-                    if(index < pathPosition || index >= pathPosition + 5){
-                        colElem.style.backgroundColor = '#42FF33';
-                
-                    }else{
-                        colElem.style.backgroundColor = '#FFFFFF';
-                    }
-        
-                    colElem.dataset.pathPosition = pathPosition;
-                    colElem.style.minWidth = colWidth + 'px';
-                    colElem.style.display = 'inline-block';
-                    colElem.style.minHeight = rowHeight + 'px';
-                    
-        
-                    colIn++;
-        
-                    c.append(colElem);
-                    
-                    
-                }
-                
-                
-        
-                rowIndex++;
-            }
-        
-            }, 100);*/
-            
         
 
         
-
         
         
     }
