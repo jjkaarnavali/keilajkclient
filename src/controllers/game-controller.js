aa6a0efd940abe244ebd0476d1fc9f1234ce57d2
jakaar <jakaar@ttu.ee>
2021-03-05 08:47:53 +0200

    MVC animation works, 1 day late

--- src/controllers/game-controller.js
+++ src/controllers/game-controller.js
@@ -1,3 +1,63 @@
+function animate(rowIndex, content, colCount){
+    setTimeout(() => {
+        
+        let colIn = 20;
+        let pathWidth = 5;
+        let rowCount = 20;
+
+        let viewPortHeight = window.innerHeight - 21;
+        let rowHeight = viewPortHeight / 20;
+        let colWidth = window.innerWidth / colCount;
+
+        let pathPosition = Number(content.lastElementChild.lastElementChild.dataset.pathPosition);
+        console.log(content.lastElementChild.lastElementChild);
+        let pathChange = Math.random() * (pathWidth - 3);
+        pathChange = Math.ceil((Math.random() * 2 - 1.5)) * pathChange;
+        if(pathPosition + pathChange + pathWidth >= rowCount || pathPosition + pathChange < 1){
+            pathPosition = pathPosition;
+        }else{
+            pathPosition = pathPosition + pathChange;
+        }
+        
+        
+
+        for (let index = 0; index < rowCount; index++) {
+            let c = content.getElementsByClassName('row-' + index)[0];
+            
+            c.firstElementChild.remove();
+            
+
+            let colElem = document.createElement('div');
+            colElem.classList.add('id-row-' + index +'-col-' + colIn);
+            
+            
+            
+
+            if(index < pathPosition || index >= pathPosition + pathWidth){
+                colElem.style.backgroundColor = '#42FF33';
+        
+            }else{
+                colElem.style.backgroundColor = '#FFFFFF';
+            }
+
+            colElem.dataset.pathPosition = pathPosition;
+            colElem.style.minWidth = colWidth + 'px';
+            colElem.style.display = 'inline-block';
+            colElem.style.minHeight = rowHeight + 'px';
+            
+
+            colIn++;
+
+            c.append(colElem);
+            
+            
+        }
+        
+        rowIndex++;
+        if(rowIndex < 200) animate(rowIndex, content, colCount);
+    }, 100);
+}
+
 export default class GameController {
 
     constructor(model, viewContainer) {
@@ -5,6 +65,7 @@ export default class GameController {
         this.model = model;
         this.isRunning = false;
     }
+    
     
     run() {
         this.isRunning = true;
@@ -19,13 +80,16 @@ export default class GameController {
 
         // start game-loop
         let rowIndex = 100;
+        animate(rowIndex, this.viewContainer.firstElementChild, this.model.colCount);
+        /*let rowIndex = 20;
 
-        while (rowIndex < 200) {
-            let viewPortHeight = window.innerHeight;
+        
+            let viewPortHeight = window.innerHeight - 21;
             let rowHeight = viewPortHeight / 20;
             let colWidth = window.innerWidth / this.model.colCount;
             let pathPosition = this.viewContainer.firstElementChild.lastElementChild.lastElementChild.dataset.pathPosition;
             setTimeout(() => {
+                while (rowIndex < 50) {
                 
                 let colIn = 20;
         
@@ -49,7 +113,7 @@ export default class GameController {
                     if(c.firstElementChild !== null){
                         c.firstElementChild.remove();
                     }
-                    //c.firstElementChild.remove();
+                    
                     
         
                     let colElem = document.createElement('div');
@@ -80,11 +144,12 @@ export default class GameController {
                 
                 
         
-
+                rowIndex++;
+            }
         
-            }, 100);
-            rowIndex++;
-        }
+            }, 100);*/
+            
+        
 
         
 
@@ -108,7 +173,8 @@ export default class GameController {
         let content = document.createElement('div');
         content.id = "gameboard";
 
-        let viewPortHeight = window.innerHeight;
+        let viewPortHeight = window.innerHeight - 21; // don't count the space under controlview
+        console.log(viewPortHeight);
         let rowHeight = viewPortHeight / 20;
         let colWidth = window.innerWidth / this.model.colCount;
         let rowId = 0;
