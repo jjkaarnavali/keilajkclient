4338d36bfee2ff62057579e4c412302ef24d764d
jakaar <jakaar@ttu.ee>
2021-03-18 20:39:22 +0200

    scoreboard dictionary doesnt save old scores

--- src/controllers/statistics-controller.js
+++ src/controllers/statistics-controller.js
@@ -1,15 +1,56 @@
-
+function findGetParameter(parameterName) {
+    var result = null,
+        tmp = [];
+    location.search
+        .substr(1)
+        .split("&")
+        .forEach(function (item) {
+          tmp = item.split("=");
+          if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+        });
+    return result;
+}
 export default class StatisticsController {
 
     constructor(model, viewContainer) {
         this.viewContainer = viewContainer;
         this.model = model;
         this.isRunning = false;
+        this.scoreBoard = {};
     }
+
+    
 
     run(){
         this.isRunning = true;
         this.viewContainer.innerHTML = 'stats';
+
+
+
+        
+        let userName = findGetParameter('userName');
+        console.log(userName);
+        let score = findGetParameter('score');
+        console.log(score);
+        if (userName !== null && score !== null) {
+            this.scoreBoard[userName] = score;
+            console.log(this.scoreBoard);
+        }
+        
+
+        if (this.scoreBoard[0] !== "") {
+            this.viewContainer.innerHTML = '';
+            for(var key in this.scoreBoard) {
+                var value = this.scoreBoard[key];
+                this.viewContainer.innerHTML += "user: " + key + " score: " + value;
+                
+              }
+        }
+
+        
+          
+
+
 
         /*function formSubmitHandler(e){
             console.log(e);
