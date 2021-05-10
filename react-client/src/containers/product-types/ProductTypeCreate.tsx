b6fa314b43f7bc5ecbc395c27d7de8226325f96a
jakaar <jakaar@ttu.ee>
2021-05-10 17:48:26 +0300

    can create new product type, but that logs user out

--- react-client/src/containers/product-types/ProductTypeCreate.tsx
+++ react-client/src/containers/product-types/ProductTypeCreate.tsx
@@ -1,7 +1,113 @@
-const ProductTypeCreate = () => {
+import { Redirect, useParams } from "react-router-dom";
+import React, { useContext, useEffect, useState } from "react";
+import { IRouteId } from "../../types/IRouteId";
+import { Link } from "react-router-dom";
+import Loader from "../../components/Loader";
+import { AppContext } from "../../context/AppContext";
+import { IProductType } from "../../dto/IProductType";
+import { BaseService } from "../../services/base-service";
+import { EPageStatus } from "../../types/EPageStatus";
+import { Route, Switch } from 'react-router-dom';
+
+interface IFormValues {
+    input: string;
+}
+export interface IFormProps {
+    values: IFormValues;
+
+    handleChange: (target: HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement) => void;
+}
+const ProductTypeCreateView = (props: IFormProps) => {
     return (
-        <div>ProductTypeCreate</div>
+        <>
+        <h1>Create</h1>
+
+        <h4>Product type</h4>
+        <hr />
+        <form>
+        <div className="row">
+            <div className="col-md-4">
+                <div className="form-group">
+                    <label htmlFor="typeName">Product type</label>
+                    <input
+                        value={props.values.input}
+                        className="form-control"
+                        onChange={(e) => props.handleChange(e.target)}
+                        type="text"
+                        id="typeName"
+                    />
+                </div>
+                <div className="form-group">
+                    <button
+                        type="submit"
+                        className="btn btn-primary"
+                    >
+                        Create
+                    </button>
+                </div>
+            </div>
+        </div>
+        </form>
+        <div>
+            <Link to={'/ProductTypes/'}>Back to List</Link> 
+        </div>
+        </>
     );
 }
 
+const initialFormValues: IFormValues = {
+    input: ''
+};
+
+const ProductTypeCreate = () => {
+    const [formValues, setFormValues] = useState(initialFormValues);
+
+    const handleChange = (target: HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement) => {
+        // todo: update form value
+        console.log(target);
+        console.log(target.value);
+        console.log(target.id);
+        setFormValues({ ...formValues, input: target.value });
+        console.log(formValues.input);
+    }
+
+    const [productType, setProductType] = useState({} as IProductType);
+
+    const [pageStatus, setPageStatus] = useState({ pageStatus: EPageStatus.Loading, statusCode: -1 });
+    const appState = useContext(AppContext);
+    productType.typeName = formValues.input;
+    console.log("mis on");
+    console.log(formValues.input);
+    const objToCreate: IProductType = {
+        id:
+        undefined,
+        typeNameId:
+        undefined,
+        typeName:
+        formValues.input
+    };
+
+    const postData = async () => {
+
+        console.log(appState.jwt);
+        let result = await BaseService.post<IProductType>(objToCreate, '/producttypes', appState.jwt!);
+        if (result.ok && result.data) {
+            setPageStatus({ pageStatus: EPageStatus.OK, statusCode: 0 });
+            setProductType(result.data);
+        } else {
+            setPageStatus({ pageStatus: EPageStatus.Error, statusCode: result.statusCode });
+        }
+    }
+
+    onsubmit = () =>{
+        
+        postData();
+        return <Route path="/producttypes"/>
+    }
+
+    
+
+
+    return <ProductTypeCreateView values={formValues} handleChange={handleChange} />
+}
 export default ProductTypeCreate;
