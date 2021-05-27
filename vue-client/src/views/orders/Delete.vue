54ad33bc432d3921ae7dfb16c0886c80089e6ccc
jakaar <jakaar@ttu.ee>
2021-05-27 11:33:02 +0300

    added order

---
+++ vue-client/src/views/orders/Delete.vue
@@ -0,0 +1,118 @@
+<template>
+<h1>Delete</h1>
+
+<h3>Are you sure you want to delete this?</h3>
+<div>
+    <h4>Order</h4>
+    <hr />
+    <dl class="row">
+        <dt class = "col-sm-2">
+            User Id
+        </dt>
+        <dd class = "col-sm-10">
+            {{order.userId}}
+        </dd>
+        <dt class = "col-sm-2">
+            Id
+        </dt>
+        <dd class = "col-sm-10">
+            {{order.id}}
+        </dd>
+    </dl>
+
+    <div class="form-group">
+        <button @click="deleteClicked($event)" type="submit" class="btn btn-danger">Delete</button>|
+        <router-link :to="'/orders/'">Back to List</router-link>
+    </div>
+</div>
+</template>
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import store from "@/store/index";
+import { BaseService } from "@/services/base-service";
+import { IOrder } from "@/domain/IOrder";
+
+@Options({
+    components: {},
+    props: {
+        id: String,
+    },
+})
+export default class OrdersDelete extends Vue {
+    id!: string;
+    order: IOrder = {
+        id: "",
+        userId: ""
+    };
+
+    async deleteClicked(event: Event): Promise<void> {
+        const service = new BaseService<IOrder>(
+            "https://localhost:5001/api/v1/Orders",
+            store.state.token ? store.state.token : undefined
+        );
+
+        const objToDelete: IOrder = {
+            id:
+            this.order.id,
+            userId:
+            this.order.userId
+        };
+
+        console.log(objToDelete);
+
+        const response = await service.delete(objToDelete);
+
+        console.log(response);
+    }
+
+    beforeCreate(): void {
+        console.log("beforeCreate");
+    }
+
+    created(): void {
+        console.log("created");
+    }
+
+    beforeMount(): void {
+        console.log("beforeMount");
+    }
+
+    mounted(): void {
+        console.log("mounted", store.state.token);
+        const service = new BaseService<IOrder>(
+            "https://localhost:5001/api/v1/Orders",
+            store.state.token ? store.state.token : undefined
+        );
+        console.log(this.id);
+        console.log("lmao");
+        service.get(this.id).then((data) => {
+            this.order.id = data.data!.id;
+            this.order.userId = data.data!.userId;
+        });
+    }
+
+    beforeUpdate(): void {
+        console.log("beforeUpdate");
+    }
+
+    updated(): void {
+        console.log("updated");
+    }
+
+    activated(): void {
+        console.log("activated");
+    }
+
+    deactivated(): void {
+        console.log("deactivated");
+    }
+
+    beforeUnmount(): void {
+        console.log("beforeUnmount");
+    }
+
+    unmounted(): void {
+        console.log("unmounted");
+    }
+}
+</script>
---
+++ vue-client/src/views/orders/Edit.vue
@@ -0,0 +1,121 @@
+<template>
+<h1>Edit order</h1>
+
+    <h4>Edit</h4>
+    <hr />
+    <div class="row">
+        <div class="col-md-4">
+            <div class="form-group">
+                <label  for="userId">User ID</label>
+                <input
+                    class="form-control"
+                    id="Input_UserId"
+                    v-model="order.userId"
+                />
+            </div>
+            <div class="form-group">
+                <button
+                    @click="saveClicked($event)"
+                    type="submit"
+                    class="btn btn-primary"
+                >
+                    Save
+                </button>
+            </div>
+        </div>
+    </div>
+
+    <div>
+        <router-link :to="'/orders/'">Back to List</router-link>
+    </div>
+</template>
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import store from "@/store/index";
+import { BaseService } from "@/services/base-service";
+import { IOrder } from "@/domain/IOrder";
+
+@Options({
+    components: {},
+    props: {
+        id: String,
+    },
+})
+export default class OrdersEdit extends Vue {
+    id!: string;
+    order: IOrder = {
+        id: "",
+        userId: ""
+    };
+
+    async saveClicked(event: Event): Promise<void> {
+        const service = new BaseService<IOrder>(
+            "https://localhost:5001/api/v1/Orders",
+            store.state.token ? store.state.token : undefined
+        );
+
+        const objToEdit: IOrder = {
+            id:
+            this.order.id,
+            userId:
+            this.order.userId
+        };
+
+        console.log(objToEdit);
+
+        const response = await service.put(objToEdit);
+
+        console.log(response);
+    }
+
+    beforeCreate(): void {
+        console.log("beforeCreate");
+        const service = new BaseService<IOrder>(
+            "https://localhost:5001/api/v1/Orders",
+            store.state.token ? store.state.token : undefined
+        );
+        console.log(this.id);
+        console.log("lmao");
+        service.get(this.id).then((data) => {
+            this.order.id = data.data!.id;
+            this.order.userId = data.data!.userId;
+        });
+    }
+
+    created(): void {
+        console.log("created");
+    }
+
+    beforeMount(): void {
+        console.log("beforeMount");
+    }
+
+    mounted(): void {
+        console.log("mounted", store.state.token);
+    }
+
+    beforeUpdate(): void {
+        console.log("beforeUpdate");
+    }
+
+    updated(): void {
+        console.log("updated");
+    }
+
+    activated(): void {
+        console.log("activated");
+    }
+
+    deactivated(): void {
+        console.log("deactivated");
+    }
+
+    beforeUnmount(): void {
+        console.log("beforeUnmount");
+    }
+
+    unmounted(): void {
+        console.log("unmounted");
+    }
+}
+</script>
